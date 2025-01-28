7cdcf062240c7f4ed9e53c92636704cdf87fff3c
fadliahmad <fadliahmad.j@gmail.com>
2025-01-28 21:34:59 +0700

    Add new project, movie transcripts

--- web_scraping_scrapy/web_scraping_scrapy/spiders/transcripts.py
+++ web_scraping_scrapy/web_scraping_scrapy/spiders/transcripts.py
@@ -6,17 +6,20 @@ from scrapy.spiders import CrawlSpider, Rule
 class TranscriptsSpider(CrawlSpider):
     name = "transcripts"
     allowed_domains = ["subslikescript.com"]
-    start_urls = ["https://subslikescript.com"]
+    start_urls = ["https://subslikescript.com/movies"]
 
     custom_settings = {
         'DOWNLOAD_DELAY': 0.5,  # Sets a download delay of 0.5 seconds
     }
 
-    rules = (Rule(LinkExtractor(allow=r"Items/"), callback="parse_item", follow=True),)
+    rules = (Rule(LinkExtractor(restrict_xpaths='//ul[@class="scripts-list"]/li/a'), callback="parse_item", follow=True),)
 
     def parse_item(self, response):
-        item = {}
-        #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
-        #item["name"] = response.xpath('//div[@id="name"]').get()
-        #item["description"] = response.xpath('//div[@id="description"]').get()
-        return item
+        article = response.xpath('//article[@class="main-article"]')
+        
+        yield { 
+            'title': article.xpath('./h1/text()').get(), 
+            'plot': article.xpath('./p[@class="plot"]/text()').get(), 
+            'transcript': article.xpath('./div[@class="full-script"]/div/p/text()').getall(),
+            'url': response.url, 
+        }
