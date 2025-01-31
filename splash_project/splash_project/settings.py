a1df424c1278484f00ac34ad00eeed7a58d19504
fadliahmad <fadliahmad.j@gmail.com>
2025-01-31 23:23:20 +0700

    Add new Splash Project in Scrapy

--- splash_project/splash_project/settings.py
+++ splash_project/splash_project/settings.py
@@ -93,6 +93,7 @@ FEED_EXPORT_ENCODING = "utf-8"
 
 #Set Splash in our Scrapy
 SPLASH_URL = 'http://192.168.59.103:8050'
+# SPLASH_URL = 'http://0.0.0.0:8050'
 
 DOWNLOADER_MIDDLEWARES = {
     'scrapy_splash.SplashCookiesMiddleware': 723,
@@ -105,5 +106,4 @@ SPIDER_MIDDLEWARES = {
 }
 
 # DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
-
 REQUEST_FINGERPRINTER_CLASS = 'scrapy_splash.SplashRequestFingerprinter'
